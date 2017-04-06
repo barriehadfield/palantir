51b8389c81cd1717f93f81f1c633a56cbc4da1bf
Barrie Hadfield <barrie@workshare.com>
2017-04-06 17:02:09 +0100

    some layout

--- components.rb
+++ components.rb
@@ -1,23 +1,31 @@
 class TopLevelComponent < Hyperloop::Component
-  render(DIV) do
-    h1 { "Time to Compare" }
-    FileChooser()
-    CompareButton()
-    ChangeTree()
-    ComparisonDocument()
+  render do
+    div.container {
+      h1 { "Time to Compare" }
+      FileChooser()
+      br
+      CompareButton()
+      br
+      div.row {
+        div.col_md_4 { ChangeTree() }
+        div.col_md_8 { ComparisonDocument() }
+      }
+    }
   end
 end
 
+
+
 class FileChooser < Hyperloop::Component
-  render(DIV) do
+  render do
     form(id:"choose", enctype: "multipart/form-data", role: "form") {
-      P {
-        p {"File number ONE"}
-        input(id: "file1", type: "file", name: "file1")
-      }
-      P {
-        p {"And the next one"}
-        input(id: "file2", type: "file", name: "file2")
+      div.row {
+        div.col_md_6 {
+          input(id: "file1", type: "file", name: "file1")
+        }
+        div.col_md_6 {
+          input(id: "file2", type: "file", name: "file2")
+        }
       }
     }
   end
@@ -25,7 +33,7 @@ end
 
 class CompareButton < Hyperloop::Component
   render(DIV) do
-    button() { "Compare" }.on(:click) do
+    button.btn.btn_primary { "Compare" }.on(:click) do
       puts "about to compare"
       `window.startComparison('comparison-document', 'changes-tree');`
     end
